7a4fcd999a83210fe68b8a2d1f752167355a6859
David Mannes <git@davidmannes.de>
2023-05-10 18:52:53 +0200

    Fix building placement enabled when starting the game

--- src/TowerBuilder.cs
+++ src/TowerBuilder.cs
@@ -9,10 +9,16 @@ public partial class TowerBuilder : Sprite2D
 	[Export]
 	public Node TowerParent { get; set; }
 
+	public override void _Ready()
+	{
+		SetEnabled(false);
+	}
+
 	public void StartBuildMode(PackedScene towerScene, Texture2D ghostTexture)
 	{
 		Texture = ghostTexture;
 		_towerScene = towerScene;
+		SetEnabled(true);
 	}
 
 	public override void _UnhandledInput(InputEvent @event)
@@ -23,11 +29,18 @@ public partial class TowerBuilder : Sprite2D
 			tower.GlobalPosition = GlobalPosition;
 			TowerParent.AddChild(tower);
 			Texture = null;
+			SetEnabled(false);
 		}
 	}
 
 	public override void _Process(double delta)
 	{
 		GlobalPosition = GetGlobalMousePosition();
+	}
+
+	private void SetEnabled(bool enabled)
+	{
+		SetProcess(enabled);
+		SetProcessUnhandledInput(enabled);
 	}
 }
