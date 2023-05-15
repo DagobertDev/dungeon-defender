9a32ced780e6b346622d1d23343fb270f844025c
David Mannes <git@davidmannes.de>
2023-05-15 20:47:46 +0200

    Add gold label

---
+++ src/GoldLabel.cs
@@ -0,0 +1,20 @@
+﻿using Godot;
+
+namespace DungeonDefender;
+
+public partial class GoldLabel : Label
+{
+	[Export]
+	private GoldComponent GoldComponent { get; set; }
+
+	public override void _Ready()
+	{
+		Require.NotNull(GoldComponent);
+		GoldComponent.CurrentGoldChanged += SetGold;
+	}
+
+	public void SetGold(int gold)
+	{
+		Text = $"Gold: {gold,-5}";
+	}
+}
