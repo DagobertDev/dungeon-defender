7a3aeca5f8da9c15e8a219daf1533f2c31961361
David Mannes <git@davidmannes.de>
2023-05-20 14:50:34 +0200

    Add arrow and arrow tower

---
+++ src/Projectiles/Arrow.cs
@@ -0,0 +1,57 @@
+﻿using DungeonDefender.Enemies;
+using Godot;
+
+namespace DungeonDefender.Projectiles;
+
+public partial class Arrow : Node2D, IProjectile
+{
+	[Export]
+	private Area2D _area;
+
+	private int _damage;
+	private Vector2 _targetPosition;
+
+	[Export(PropertyHint.Range, "0, 1000, or_greater")]
+	public int Speed { get; private set; }
+
+	public void FireAt(Tower tower, Enemy target)
+	{
+		_targetPosition = target.Position;
+		_damage = tower.Damage;
+		Position = tower.Position;
+		tower.GetParent().AddChild(this);
+	}
+
+	public override void _Ready()
+	{
+		Require.MoreThanZero(Speed);
+		Require.NotNull(_targetPosition);
+		Require.NotNull(_area);
+		_area.AreaEntered += OnCollision;
+	}
+
+	public override void _Process(double delta)
+	{
+		Position = Position.MoveToward(_targetPosition, Speed * (float)delta);
+		Rotation = Position.AngleToPoint(_targetPosition);
+
+		if (Position.DistanceSquaredTo(_targetPosition) < 1)
+		{
+			QueueFree();
+		}
+	}
+
+	private void OnCollision(Area2D area)
+	{
+		if (area.GetParent() is Enemy enemy)
+		{
+			OnEnemyHit(enemy);
+		}
+	}
+
+	private void OnEnemyHit(Enemy enemy)
+	{
+		enemy.Health.ApplyDamage(_damage);
+		QueueFree();
+	}
+}
